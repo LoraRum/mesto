0f2c390186bc0ef455f89a801edcdc8fa4bc00ff
Laura Rumiantseva <gromina.l.d@gmail.com>
2022-12-05 18:07:17 +0600

    save text and rename class

--- scripts/index.js
+++ scripts/index.js
@@ -3,53 +3,43 @@ const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const popupContainer = document.querySelector('.popup__container');
 const popupSave = document.querySelector('.popup__save');
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.input_tape_name');
+let jobInput = formElement.querySelector('.input_tape_about');
+
+let name = document.querySelector('.profile__title');
+let about = document.querySelector('.profile__subtitle');
 
 //Открыть попап
-
 profileButton.addEventListener('click', openPopup)
-
-function openPopup() {
-    popup.classList.add('popup_opened');
-}
-
 //Закрыть попап
-
 popupClose.addEventListener('click', closePopup)
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-
 //Закрыть попап нажатием на экран
-
 popup.addEventListener('click', function (event) {
     closePopup();
 });
 popupContainer.addEventListener('click', function (event) {
     event.stopPropagation()
 });
+formElement.addEventListener('submit', handleFormSubmit);
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
+function openPopup() {
+    popup.classList.add('popup__opened');
+    nameInput.value = name.textContent;
+    jobInput.value = about.textContent;
+}
 
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-about');
-
-let name = document.querySelector('.profile__title');
-let about = document.querySelector('.profile__subtitle');
+function closePopup() {
+    popup.classList.remove('popup__opened');
+}
 
 // Обработчик «отправки» формы
-
 function handleFormSubmit(event) {
     event.preventDefault();
     name.textContent = nameInput.value;
     about.textContent = jobInput.value;
-
-    nameInput.value = name.textContent;
-    jobInput.value = about.textContent;
-
     closePopup();
 }
-
-formElement.addEventListener('submit', handleFormSubmit);
