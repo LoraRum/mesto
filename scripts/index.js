19c08ef199ecfd6f764f3fb68ed454eb3b206db0
Laura Rumiantseva <gromina.l.d@gmail.com>
2023-01-01 23:58:40 +0600

    del like

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,21 @@ const initialCards = [
     {
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-        like: false,
     }, {
         name: 'Челябинская область',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-        like: false,
     }, {
         name: 'Иваново',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-        like: false,
     }, {
         name: 'Камчатка',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-        like: false,
     }, {
         name: 'Холмогорский район',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-        like: false,
     }, {
         name: 'Байкал',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-        like: false,
     },
 ];
 const userProfileButton = document.querySelector('.profile__edit-button');
