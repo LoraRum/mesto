da1d0f72d9cf72bd6f5998f0686c97be0a7f5c3d
Laura Rumiantseva <gromina.l.d@gmail.com>
2023-03-01 20:57:00 +0600

    аватар и попап аватара

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,10 @@ import UserInfo from '../components/UserInfo.js';
 
 const userProfileButton = document.querySelector('.profile__edit-button');
 const newPlaceButton = document.querySelector('.profile__add-button');
+const avatarImageButton = document.querySelector('.avatar')
 const userProfileForm = document.querySelector('#popup-user-profile .form__form');
 const newPlaceForm = document.querySelector('#popup-new-place .form__form');
+const avatarImageForm = document.querySelector('#avatar .form__form')
 const userNameInput = userProfileForm.querySelector('#username');
 const userAboutInput = userProfileForm.querySelector('#about');
 
@@ -20,6 +22,22 @@ const newPlaceFormValidator = new FormValidator(newPlaceForm, validationConfig);
 
 const popupUserProfile = new PopupWithForm('#popup-user-profile', {onSubmit: handleUserProfileFormSubmit});
 const popupNewPlace = new PopupWithForm('#popup-new-place', {onSubmit: handleNewPlaceFormSubmit});
+const popupAvatarImage = new PopupWithForm('#popup-change-avatar', {
+    onSubmit: () => {
+
+        fetch('https://mesto.nomoreparties.co/v1/cohort-60/users/me/avatar', {
+            method: 'PATCH',
+            headers: {
+                authorization: '6059afea-f832-4b2c-a73d-15748b82d9cd',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                avatar: ''
+            })
+        });
+
+    }
+});
 const popupFullScreen = new PopupWithImage('#popup-fullscreen');
 const cardsSection = new Section({items: initialCards, renderer: createCard}, '.groups');
 const userInfo = new UserInfo({
@@ -37,12 +55,19 @@ newPlaceButton.addEventListener('click', () => {
     popupNewPlace.open();
 });
 
+avatarImageButton.addEventListener('click', () => {
+    popupAvatarImage.open();
+});
+
+
 userProfileFormValidator.enableValidation();
 newPlaceFormValidator.enableValidation();
+
 
 popupUserProfile.setEventListeners();
 popupNewPlace.setEventListeners();
 popupFullScreen.setEventListeners();
+popupAvatarImage.setEventListeners()
 
 
 function fillUserProfileForm() {
@@ -63,3 +88,6 @@ function handleUserProfileFormSubmit(data) {
 function handleNewPlaceFormSubmit(data) {
     cardsSection.addItem(data);
 }
+
+
+
